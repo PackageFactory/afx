479f28c68e426a0abefe591931f406a4bbc034f3
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-12-26 23:38:00 +0100

    feat(tests): Test more complex nesting scenarios

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -131,4 +131,132 @@ class ParserTest extends TestCase
 			]
 		], $parser->parse());
 	}
+
+	/**
+	 * @test
+	 */
+	public function shouldParseNestedTags()
+	{
+		$parser = new Parser('<article><header><div>Header</div></header><div>Content</div><footer><div>Footer</div></footer></article>');
+		$this->assertEquals([
+			'identifier' => 'article',
+			'props' => [],
+			'children' => [
+				[
+					'type' => 'node',
+					'payload' => [
+						'identifier' => 'header',
+						'props' => [],
+						'children' => [
+							[
+								'type' => 'node',
+								'payload' => [
+									'identifier' => 'div',
+									'props' => [],
+									'children' => [
+										[
+											'type' => 'text',
+											'payload' => 'Header'
+										]
+									]
+								]
+							]
+						]
+					]
+				],
+				[
+					'type' => 'node',
+					'payload' => [
+						'identifier' => 'div',
+						'props' => [],
+						'children' => [
+							[
+								'type' => 'text',
+								'payload' => 'Content'
+							]
+						]
+					]
+				],
+				[
+					'type' => 'node',
+					'payload' => [
+						'identifier' => 'footer',
+						'props' => [],
+						'children' => [
+							[
+								'type' => 'node',
+								'payload' => [
+									'identifier' => 'div',
+									'props' => [],
+									'children' => [
+										[
+											'type' => 'text',
+											'payload' => 'Footer'
+										]
+									]
+								]
+							]
+						]
+					]
+				]
+			]
+		], $parser->parse());
+	}
+
+	/**
+	 * @test
+	 */
+	public function shouldHandletWhitespace()
+	{
+		$parser = new Parser('   <div>
+							<input/>
+					<label>Some
+
+					Text</label>
+							     </div>   ');
+		$this->assertEquals([
+			'identifier' => 'div',
+			'props' => [],
+			'children' => [
+				[
+					'type' => 'text',
+					'payload' => '
+							'
+				],
+				[
+					'type' => 'node',
+					'payload' => [
+						'identifier' => 'input',
+						'props' => [],
+						'children' => []
+					]
+				],
+				[
+					'type' => 'text',
+					'payload' => '
+					'
+				],
+				[
+					'type' => 'node',
+					'payload' => [
+						'identifier' => 'label',
+						'props' => [],
+						'children' => [
+							[
+								'type' => 'text',
+								'payload' => 'Some
+
+					Text'
+							]
+						]
+					]
+				],
+				[
+					'type' => 'text',
+					'payload' => '
+							     '
+				]
+			]
+		], $parser->parse());
+	}
 }
