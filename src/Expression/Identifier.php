3b92b4d5bc4d24e05bcb22aa32bce2e119c9cc16
Martin Ficzel <ficzel@sitegeist.de>
2017-04-25 09:07:43 +0200

    refactor: make parsing of identifiers more rigid and throw an exception if unexpected characters occur

--- src/Expression/Identifier.php
+++ src/Expression/Identifier.php
@@ -1,6 +1,7 @@
 <?php
 namespace PackageFactory\Afx\Expression;
 
+use PackageFactory\Afx\Exception;
 use PackageFactory\Afx\Lexer;
 
 class Identifier
@@ -9,16 +10,30 @@ class Identifier
     {
         $identifier = '';
 
-        while ($lexer->isAlphaNumeric() ||
-            $lexer->isDot() ||
-            $lexer->isColon() ||
-            $lexer->isMinus() ||
-            $lexer->isUnderscore() ||
-            $lexer->isAt()
-        ) {
-            $identifier .= $lexer->consume();
+        while (true) {
+            switch (true) {
+                case $lexer->isAlphaNumeric():
+                case $lexer->isDot():
+                case $lexer->isColon():
+                case $lexer->isMinus():
+                case $lexer->isUnderscore():
+                case $lexer->isAt():
+                    $identifier .= $lexer->consume();
+                    break;
+                case $lexer->isEqualSign():
+                case $lexer->isWhiteSpace():
+                case $lexer->isClosingBracket():
+                case $lexer->isForwardSlash():
+                    return $identifier;
+                    break;
+                default:
+                    $unexpected_character = $lexer->consume();
+                    throw new Exception(sprintf(
+                        'Unexpected character "%s" in identifier "%s"',
+                        $unexpected_character,
+                        $identifier
+                    ));
+            }
         }
-
-        return $identifier;
     }
 }
